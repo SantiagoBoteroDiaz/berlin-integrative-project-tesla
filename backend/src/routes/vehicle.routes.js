e3c88f959fac1696892434ca86b7060e98ae03d5
Steven-Patino <stevenpat27@gmail.com>
2026-03-16 19:49:55 -0500

    remove unused registExitAndPayment route and clean up vehicle routes

--- backend/src/routes/vehicle.routes.js
+++ backend/src/routes/vehicle.routes.js
@@ -7,13 +7,8 @@ import {
   hourlyRate,
   suscription,
   registNew,
-<<<<<<< HEAD
-  registPlan,
-  registExitAndPayment
-=======
   registPlan, 
   getDashboard
->>>>>>> 7f0445310308728756fd6a07d478c37e9e71a107
 } from "../controllers/vehicle.controller.js";
 import { confirmPayment, handlePaymentCallback } from "../controllers/vehicle.controller.js"; 
 const vehicleRoutes = Router();
@@ -30,10 +25,6 @@ vehicleRoutes.get('/hourlyRate', hourlyRate);
 vehicleRoutes.get('/suscription', suscription); 
 vehicleRoutes.post('/newVehicle' , registNew);   
 vehicleRoutes.post('/registPlan', registPlan); 
-<<<<<<< HEAD
-vehicleRoutes.post('/exitPermission', registExitAndPayment);
-=======
 vehicleRoutes.get('/dashboard', getDashboard); 
->>>>>>> 7f0445310308728756fd6a07d478c37e9e71a107
 
 export default vehicleRoutes;
